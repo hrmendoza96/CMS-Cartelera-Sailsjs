b6a11d6faf0363b057173633c1e0418a1d77d819
hrmendoza96 <hrmendoza96@unitec.edu>
2019-11-20 15:03:49 -0600

    Reestableciendo

--- cms-cartelera/config/session.js
+++ cms-cartelera/config/session.js
@@ -11,29 +11,29 @@
 
 module.exports.session = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Session secret is automatically generated when your new app is created   *
-  * Replace at your own risk in production-- you will invalidate the cookies *
-  * of your users, forcing them to log in again.                             *
-  *                                                                          *
-  ***************************************************************************/
-  secret: '3d228b810502a03c5f77505f4f064e09',
+    /***************************************************************************
+     *                                                                          *
+     * Session secret is automatically generated when your new app is created   *
+     * Replace at your own risk in production-- you will invalidate the cookies *
+     * of your users, forcing them to log in again.                             *
+     *                                                                          *
+     ***************************************************************************/
+    secret: '3d228b810502a03c5f77505f4f064e09',
 
 
-  /***************************************************************************
-  *                                                                          *
-  * Customize when built-in session support will be skipped.                 *
-  *                                                                          *
-  * (Useful for performance tuning; particularly to avoid wasting cycles on  *
-  * session management when responding to simple requests for static assets, *
-  * like images or stylesheets.)                                             *
-  *                                                                          *
-  * https://sailsjs.com/config/session                                       *
-  *                                                                          *
-  ***************************************************************************/
-  // isSessionDisabled: function (req){
-  //   return !!req.path.match(req._sails.LOOKS_LIKE_ASSET_RX);
-  // },
+    /***************************************************************************
+     *                                                                          *
+     * Customize when built-in session support will be skipped.                 *
+     *                                                                          *
+     * (Useful for performance tuning; particularly to avoid wasting cycles on  *
+     * session management when responding to simple requests for static assets, *
+     * like images or stylesheets.)                                             *
+     *                                                                          *
+     * https://sailsjs.com/config/session                                       *
+     *                                                                          *
+     ***************************************************************************/
+    // isSessionDisabled: function (req){
+    //   return !!req.path.match(req._sails.LOOKS_LIKE_ASSET_RX);
+    // },
 
 };
